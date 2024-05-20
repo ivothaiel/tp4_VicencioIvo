c5bab898573d217becd9dafedd89132ed4352b6b
admin <admin@192.168.1.11>
2024-05-20 18:31:29 -0300

    inicio del tp4

---
+++ tp4_VicencioIvo/src/test/java/ejemplo/Tp4VicencioIvoApplicationTests.java
@@ -0,0 +1,13 @@
+package ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4VicencioIvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
